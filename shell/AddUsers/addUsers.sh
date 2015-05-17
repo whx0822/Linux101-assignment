7a8573d8e37951366a99a0647848455fe8ba8c90
root <627755295@qq.com>
2015-05-18 00:26:53 +0800

    zhushi

--- shell/AddUsers/addUsers.sh
+++ shell/AddUsers/addUsers.sh
@@ -1,9 +1,8 @@
-#
-#
-# useradd
-# chpasswd
-# chown
-# chmod
-# edquota
-#
-
+#!bin/sh
+for name in $(cat users.txt)
+	do useradd $name
+	echo "123456"|echo "123456" |passwd $name
+	mkdir $name /home/$name
+	chown $name /home/$name
+	chmod 755 /home/$name
+done
